a5f88b1901aadb4ca3680e4bd4fac6827575d57e
nbintang <nbintangh@gmail.com>
2024-07-11 14:11:41 +0700

    fix: credenrials cors to true

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import productRoutes from "./routes/product.router.js";
 import orderRoutes from "./routes/order.router.js";
 import orderItemRoutes from "./routes/order-item.router.js";
 import authRoutes from "./routes/auth.router.js";
-
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import jsonwebtoken from "jsonwebtoken";
-const { JsonWebTokenError } = jsonwebtoken;
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:5173"
+}));
+
 app.use(cookieParser());
 app.use(express.json());
 
